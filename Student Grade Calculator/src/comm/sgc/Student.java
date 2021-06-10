6245a53cf08ab5cebe82d7d0f339b573a5cb8e47
001ZT1744 <001ZT1744@LAPTOP-TG9H65L5>
2021-06-10 18:04:36 +0530

    Second Assignment commit. need some changes.

---
+++ Student Grade Calculator/src/comm/sgc/Student.java
@@ -0,0 +1,84 @@
+package comm.sgc;
+
+public class Student {
+	private int id;
+	private String name;
+	private  int[] marks;
+	private static float average;
+	private static char grade;
+	public Student() {
+		super();
+	}
+	public Student(int id, String name, int[] marks) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+	
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int[] getMarks() {
+		return marks;
+	}
+	public void setMarks(int[] marks) {
+		this.marks = marks;
+	}
+
+	public void calculateAvg()
+	{
+		int total=0;
+		for(int i=0;i<getMarks().length;i++)
+		{
+			total=total+marks[i];
+			
+		}
+		average=total/getMarks().length;
+		
+	}
+	public float getAverage()
+	{
+		return average;
+	}
+	public void findGrade()
+	{
+		int flag=0;
+		if(average>=80 && average<=100)
+		{
+			grade='O';
+			
+		}
+		else {
+			grade='A';
+		}
+		for(int i=0;i<getMarks().length;i++)
+		{
+			if(marks[i]<50)
+			{
+				flag=1;
+				break;
+			}
+			else
+				continue;
+		}
+		if(flag==1)
+		{
+			grade='F';
+		}
+	}
+	public char getGrade()
+	{
+		return grade;
+	}
+
+}
---
+++ Student Grade Calculator/src/comm/sgc/StudentMain.java
@@ -0,0 +1,51 @@
+package comm.sgc;
+import java.util.Scanner;
+
+public class StudentMain {
+	private static Scanner sc=new Scanner(System.in);
+	
+	public static Student getStudentDetails()
+	
+	{
+		
+		System.out.println("Enter the Student Id");
+		int id=sc.nextInt();
+
+		System.out.println("Enter the Student name");
+		String name=sc.next();
+		
+
+		System.out.println("Enter the number of subjects");
+		int n = sc.nextInt();
+		if(n==0)
+		{
+			System.out.println("Please enter valid number of subjects");
+		}
+		int[] mark =new int[n];
+		for(int i=0;i<n;i++)
+		{
+		System.out.println("Enter the marks for subject "+i);
+		int m=sc.nextInt();
+		if(m<=0||m>100)
+		{
+			System.out.println("Invalid Mark");
+			i--;
+			continue;
+		}
+		mark[i]=m;
+		}
+		Student student=new Student(id,name,mark);
+		return student;
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Student sm=getStudentDetails();
+//		std=getStudentDetails();
+		System.out.println("Average is "+sm.getAverage());
+		System.out.println("Grade is "+sm.getGrade());
+	}
+
+}
