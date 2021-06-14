289f39830eb97e27fe407e8c68892bf64db9916b
001ZT1744 <001ZT1744@LAPTOP-TG9H65L5>
2021-06-14 16:29:08 +0530

    CRUD operation added

---
+++ comparator/src/com/example/app/InvalidNameException.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+public class InvalidNameException extends RuntimeException {
+	private String message;
+	public InvalidNameException(String message)
+	{
+		super();
+		this.message=message;
+	}
+	public String getMessage()
+	{
+		return message;
+	}
+
+}
