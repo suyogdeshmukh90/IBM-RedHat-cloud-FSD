86834215b823aa684c13e2a34970e1ffc1c001af
suyogdeshmukh90 <81974209+suyogdeshmukh90@users.noreply.github.com>
2021-06-30 11:28:25 +0530

    commit first

---
+++ JavaScript-Example/app.js
@@ -0,0 +1,30 @@
+let name='Shawn';
+let age=27;
+name=true;
+const max=101;
+
+console.log(max);
+let num=10;
+num++;
+console.log(--num);
+
+let var1 = 9;
+let var2 = 9;
+ 
+// Equal ===
+console.log(var1 === var2);
+ 
+// Not Equal !=
+console.log(var1 != var2);
+ 
+// Greater than >
+console.log(var1 > var2);
+ 
+// Less than <
+console.log(var1 < var2);
+ 
+// Greater than or equal to >=
+console.log(var1 >= var2);
+ 
+// Less than or equal to <=
+console.log(var1 <= var2);
---
+++ JavaScript-Example/custom.js
@@ -0,0 +1,3 @@
+console.log(4+9);
+console.log(2*8);
+console.log('Hi There!');
